37448765b52d4dc6f62858a78bbe0264843ce07f
mac <trongnhan34vn@gmail.com>
2023-06-26 16:58:52 +0700

    Thich thi out di

--- src/main/java/org/example/NhanClass.java
+++ src/main/java/org/example/NhanClass.java
@@ -3,5 +3,7 @@ package org.example;
 public class NhanClass {
     public static void main(String[] args) {
         System.out.println("Hung hihi!");
+        System.out.println("Hung tech lead nhu cc hihi!");
+        System.out.println("By Nhan Tic");
     }
 }
