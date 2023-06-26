985f3481b7ad6ee207d6ad1b1172d4e91587254a
Luonghaison <keibin.wru@gmail.com>
2023-06-26 17:16:09 +0700

    2232hghgn hbnhgbh

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -2,7 +2,7 @@ package org.example;
 
 public class Main {
     public static void main(String[] args) {
-        System.out.println("MY NAME BA LAM!");
-        System.out.println("MY NAME BA Nhan!");
+        System.out.println("MY NAME hiuiuiihjihiis!");
+        System.out.println("MY NAME hs!");
     }
 }
