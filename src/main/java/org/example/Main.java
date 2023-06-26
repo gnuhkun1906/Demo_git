e620c365e16b7cb29296821ff44a9a7f3d6c818c
Nguyễn Bá Lâm <nguyenbalam2307@gmail.com>
2023-06-26 16:58:18 +0700

    balam chinh sua

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -2,5 +2,6 @@ package org.example;
 public class Main {
     public static void main(String[] args) {
         System.out.println("Anh Quang Tuấn!");
+        System.out.println("Anh là Bá Lâm so sad!");
     }
 }
