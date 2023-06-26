eacda31c11a2c26d5c97a3ee297669e0c174b663
Tô Việt Hoàng <totranhieu@gmail.com>
2023-06-26 16:14:53 +0700

    hieuhoa

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -2,6 +2,6 @@ package org.example;
 
 public class Main {
     public static void main(String[] args) {
-        System.out.println("MY NAME BA LAM!");
+        System.out.println("MY NAME BA LAM Beo!");
     }
 }
